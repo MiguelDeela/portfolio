ee1ad0c6ee5454313c4ecb046325b003086ce866
Miguel_Deela <144940112+MiguelDeela@users.noreply.github.com>
2024-02-28 21:34:07 -0300

    portafolio

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,12 +1,13 @@
 
-const Carousel = ({children}) => {
+const Carousel = ({children}: {children: React.ReactNode}) => {
 	return (
 		<li className="carousel-item animated">
-			<svg stroke="currentColor" fill="#fbbf24" stroke-width="0" role="img" viewBox="0 0 24 24" height="3em" width="3em" xmlns="http://www.w3.org/2000/svg">
+			<svg stroke="currentColor" fill="#fbbf24" strokeWidth="0" role="img" viewBox="0 0 24 24" height="3em" width="3em" xmlns="http://www.w3.org/2000/svg">
 				{children}
 			</svg>
 		</li>
 	)
+	
 }
 
 export default Carousel
